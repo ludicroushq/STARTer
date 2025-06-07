dfe33af06fe52ad22eab8269d03a9c444e232b34
Manthan Mallikarjun <nahtnam@gmail.com>
2025-06-07 13:27:37 -0700

    Remove extra file

--- src/server/types/index.ts
+++
@@ -1,6 +0,0 @@
-import type { JsonifiedValue } from "@orpc/openapi-client";
-import type { InferRouterInputs, InferRouterOutputs } from "@orpc/server";
-import { router } from "../routes";
-
-export type Inputs = JsonifiedValue<InferRouterInputs<typeof router>>;
-export type Outputs = JsonifiedValue<InferRouterOutputs<typeof router>>;
