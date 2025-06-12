5fbb7c7c7961e312fc8794a933e9f262df729369
Manthan Mallikarjun <nahtnam@gmail.com>
2025-06-11 18:01:49 -0700

    Update zod-validation-error

--- src/lib/zod-validation-error/message-builder.ts
+++ src/lib/zod-validation-error/message-builder.ts
@@ -3,4 +3,5 @@ import { createMessageBuilder } from "zod-validation-error";
 export const messageBuilder = createMessageBuilder({
   maxIssuesInMessage: 1,
   prefix: null,
+  includePath: false,
 });
