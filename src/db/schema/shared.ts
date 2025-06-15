d07895be3b71a6fc626dae9314cf102942d84d1c
Manthan Mallikarjun <nahtnam@gmail.com>
2025-06-14 18:48:21 -0700

    Update shared fields

--- src/db/schema/shared.ts
+++ src/db/schema/shared.ts
@@ -2,11 +2,12 @@ import { text, timestamp } from "drizzle-orm/pg-core";
 import { createId } from "@paralleldrive/cuid2";
 
 export const shared = {
-  createdAt: timestamp().defaultNow(),
   id: text()
     .primaryKey()
     .$defaultFn(() => createId()),
+  createdAt: timestamp().notNull().defaultNow(),
   updatedAt: timestamp()
+    .notNull()
     .defaultNow()
     .$onUpdate(() => new Date()),
 };
